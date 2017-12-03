3fd790c4c9c6edf39ca1d504c3d4109b22d625dd
xbx0119 <huangyifan0119@163.com>
2017-12-03 21:48:58 +0800

    some bugs

--- bot/bot.js
+++ bot/bot.js
@@ -3,51 +3,146 @@
  */
 
 const { Wechaty, Room } = require('wechaty')
-const Msg = require('../lib/Msg.js');
+const excel = require('../lib/excel')
 
-const ROOM_NAME = 'test';
+const ROOM_NAME = '信息2014寝室长群';
 const DEV = true;
 
+var Bot = function () {
+    // this.robot_id = 0;
+    // this.robot_master = master;
+    this.data = [];
+    this.setup();
+    
+}
 
-Wechaty.instance()
+// 总数
+// Bot.prototype.sum = 0;
 
-// 二维码
-.on('scan', (url, code) => {
+// 启动机器人
+Bot.prototype.setup = function () {
+    // this.robot_id = this.sum + 1;
+    // this.sum = this.robot_id;
+    var self = this;
+
+    Wechaty.instance()
+
+    // 二维码
+    .on('scan', (url, code) => {
+        self.dispatch({
+            mode: 'scan',
+            data: {
+                url: url
+            }
+        })
+    })
+
+    // 登录
+    .on('login', user => {
+        self.dispatch({
+            mode: 'login',
+            data: {
+                user: user
+            }
+        })
+    })
+
+    // 收到消息
+    .on('message', message => {
+        // 自己的消息
+        
+        self.dispatch({
+            mode: 'message',
+            data: {
+                message: message
+            }
+        })
+        
+        if (message.self()) {
+            return;
+        }
+
+    })
+
+    .start();
+
+    // 检测关闭
+    // var liveTime = setInterval(function() {
+
+    // }, 1000 * 60)
+}
+
+// 关闭机器人
+Bot.prototype.kill = function() {
+
+}
+
+// 调度
+Bot.prototype.dispatch = function (msg) {
+    console.log(msg)
+
+    switch (msg.mode) {
+        case 'scan':
+            this.dealWith_scan(msg.data.url);
+            break;
+        case 'login':
+            this.dealWith_login(msg.data.user);
+            break;
+        case 'message':
+            this.dealWith_message(msg.data.message);
+            break;
+        default:
+            console.log("default")
+    }
+}
+
+Bot.prototype.dealWith_scan = function (url) {
     let loginUrl = url.replace('qrcode', 'l');
-    //require('qrcode-terminal').generate(loginUrl)
-    Msg.post({
-        mode: 'scan',
-        data: {
-            url: url
-        }
-    })
-});
+    require('qrcode-terminal').generate(loginUrl)
+}
 
-// 登录
-.on('login', user => {
-    Msg.post({
-        mode: 'login',
-        data: {
-            user: user
-        }
-    })
-})
+Bot.prototype.dealWith_login = function (user) {
+    console.log(`${user} login`)
+}
 
-// 收到消息
-.on('message', message => {
-    // 自己的消息
-    if (message.self()) {
+Bot.prototype.dealWith_message = function (message) {
+    const person = message.from()
+    const content = message.content()
+    const room = message.room()
+
+    console.log("message")
+    // 群聊名称
+    if (room) {
+        var room_name = room.topic();
+        console.log(room.roomAliasMap)
+        // console.log(person)
+
+        if (room_name != ROOM_NAME) {
+
+            // console.log(person.name + ": " + content)
+            if (/^robot:\/\//g.test(content)) {
+                var info = content.slice(8).split("/");
+                this.data.push(info);
+                message.say("robot get your answer: " + content)
+            }
+        }
+        // 超过11点
+        var now = new Date();
+        var eleven = new Date(now.toLocaleString() + ' 23:00:00')
+        if(DEV || now > eleven){
+            excel(this.data);
+        }
+
+    } else {
+        // 非群聊消息，公众号或个人
         return;
     }
+}
 
-    Msg.post({
-        mode: 'message',
-        data: {
-            message: message
-        }
-    })
 
-})
+Bot.prototype.sendInfo = function() {
+    return Msg;
+}
 
-.start()
 
+module.exports = Bot;
--- routers.js
+++ routers.js
@@ -2,8 +2,38 @@ var express = require('express');
 
 var router = express.Router();
 
+var Bot = require('./bot/bot');
+
 router.get('/', function(req, res, next) {
 	res.render('index')
 });
+
+router.get('/console', function (req, res, next) {
+
+	var robot = new Bot();
+
+	res.render('console')
+});
+
+// io.on('connection', function (socket) {
+// 	console.log('socket: a user connected');
+
+// 	socket.on('disconnect', function () {
+// 		console.log('connect break');
+// 	});
+
+// 	socket.on('login', function (info) {
+// 		var data = JSON.parse(info);
+// 		if (true) {
+// 			const user = {
+// 				name: data.user,
+// 				socket: socket
+// 			};
+// 			userList.push(user);
+// 			socket.emit('login', 'true');
+// 		}
+// 	});
+// });
+
 
 module.exports = router;
